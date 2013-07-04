8f91073f31600cd4be53a4cdec816ff475453a79
Yuri <amie.yuri@gmail.com>
2013-07-04 01:13:15 -0700

    modify function and readme

--- README.md
+++ README.md
@@ -1,22 +1,21 @@
 WordCloud
 =========
 
-This R script plots a word cloud of the most frequent words 
-from tweets retrieved according to a hashtag.
+This R program plots a word cloud of the most frequent words 
+from tweets retrieved according to a search term.
 
 The main function PlotWordCloud(searchTerm, maxTweets)
-takes a search term (or hashtag) and a maximum number of tweets to retrieve,
+takes a search term and a maximum number of tweets to be retrieved; 
 and plots the word cloud.
 
-It follows four steps: 
-1 Download necessary packages.
+It consists of four steps: 
+1 Download necessary packages
 2 Get and clean tweets
 3 Calculate words' frequencies
 4 Plot word cloud
 
-For example:
-PlotWordCloud("#kitsilano", 100)
-produces the word cloud in kitsWD.svg
+Example: PlotWordCloud("#kitsilano", 100) 
+produces the word cloud in kitsWD.png
 
 
 
--- twitterFunc.R
+++ twitterFunc.R
@@ -1,4 +1,4 @@
-# EnsurePackage(x) - Installs and loads package "x" (if necessary).
+# EnsurePackage(x) - Installs and loads package "x" (if necessary)
 EnsurePackage <- function(x)
 {
   x <- as.character(x)
@@ -9,8 +9,7 @@ EnsurePackage <- function(x)
   }
 }
 
-# PrepareTwitter() - Loads packages for working with twitteR, tm, 
-# wordcloud and stringr.
+# PrepareTwitter() - Loads necessary packages 
 PrepareTwitter <- function()
 {
   EnsurePackage("bitops")
@@ -23,7 +22,7 @@ PrepareTwitter <- function()
   EnsurePackage("RColorBrewer")
 }
 
-# Enables R to retrieve tweets from twitter.
+# Enables R to retrieve tweets from twitter
 Login <- function()
 {
   load("~/OAuth.RData")
@@ -31,54 +30,48 @@ Login <- function()
 }
 
 # TweetsFrame() - Takes a search term and the maximum number of tweets.
-# Returns an ordered dataframe according to creation date
-# that contains retrieved tweets.
+# Returns a data frame with retrieved tweets
 TweetsFrame <- function(searchTerm, maxTweets)
 {
-  # Get tweets as a List.
+  # Get tweets as a List
   tweetList <- searchTwitter(searchTerm, n=maxTweets)
   
-  # Create a rectangular Data Frame out of tweetList, using:
-  # as.data.frame() - Coerces eash list element into a row
-  # lapply() - Applies this to all of the elements in tweetList
-  # rbind() - Takes all of the rows and puts them together
-  # do.call() - Gives rbind() all the rows as individual elements
+  # Create a rectangular data frame out of tweetList
   tweetDF <- do.call("rbind", lapply(tweetList,as.data.frame))
   
-  # Order tweetDF according to creation date.
+  # Order tweetDF according to creation date
   tweetDF <- tweetDF[order(as.integer(tweetDF$created)),] 
   
   return(tweetDF)
 }  
 
-# CleanTweets() - Takes a vector containig the tweets' text.
-# Returns same vector with cleaned text.
+# CleanTweets() - Takes a vector containig tweets' text and
+# returns same vector with cleaned text.
 CleanTweets<-function(tweets)
 {
-  # Remove redundant spaces.
+  # Remove redundant spaces
   tweets <- str_replace_all(tweets,"  "," ")  
-  # Get rid of URLs that contain 10 alphanumeric symbols.
-  tweets <- str_replace_all(tweets,"http[s]*://t.co/[a-z,A-Z,0-9]{10}","")
-  # Get rid of URLs that contain 7 alphanumeric symbols (and end in ellipsis).
+  # Get rid of URLs 
+  tweets <- str_replace_all(tweets,"http[s]*://t.co/[a-z,A-Z,0-9]{10}","")  
   tweets <- str_replace_all(tweets,"http[s]*://t.co/[a-z,A-Z,0-9]{7}","")  
-  # Take out retweet header, there is only one.
+  # Take out retweet header, there is only one
   tweets <- str_replace(tweets,"RT @[a-z,A-Z,0-9,_]*: ","")
-  # Get rid of hashtags.
+  # Get rid of hashtags
   tweets <- str_replace_all(tweets,"#[a-z,A-Z,0-9,_]*","")
-  # Get rid of references to other screennames.
+  # Get rid of references to other screennames
   tweets <- str_replace_all(tweets,"@[a-z,A-Z,0-9,_]*","")
-  # Get rid of &amp.
+  # Get rid of &amp
   tweets <- str_replace_all(tweets,"&amp","")
-  # Get rid of hexadecimal characters.
+  # Get rid of hexadecimal characters
   tweets <- str_replace_all(tweets,"[\x01-\x1f\x7f-\xff]","")  
   return(tweets)  
 }
 
 
 
-# PlotWordCloud() - Takes a search term and a maximum number of tweets to retrieve.
-# Plots a wordcloud of the most frequent words in these tweets. 
-# (The higher the frequency, the bigger the word).
+# PlotWordCloud() - Takes a search term and a maximum number of tweets.
+# Plots a wordcloud of the most frequent words in these tweets 
+# (the higher the frequency, the bigger the word).
 PlotWordCloud <- function(searchTerm, maxTweets)
 { 
   #-----Loading necessary packages------
@@ -88,7 +81,7 @@ PlotWordCloud <- function(searchTerm, maxTweets)
   
   #-----Getting tweets ready------
   
-  # Get tweets. 
+  # Get tweets
   tweetDF <- TweetsFrame(searchTerm, maxTweets)    
   # Clean tweets' text from hashtags, retweets, urls, etc.
   cleanText <- CleanTweets(tweetDF$text)    
@@ -96,9 +89,9 @@ PlotWordCloud <- function(searchTerm, maxTweets)
   # Convert tweets to Corpus class, 
   # this allows us to do further cleaning of data.
   tweetCorpus <- Corpus(VectorSource(cleanText))
-  # Convert text to lower case.
+  # Convert text to lower case
   tweetCorpus <- tm_map(tweetCorpus, tolower)
-  # Remove punctuation from text.
+  # Remove punctuation from text
   tweetCorpus <- tm_map(tweetCorpus, removePunctuation)
   # Remove stop words like: and, on, in, the, etc., in several languages.
   tweetCorpus <- tm_map(tweetCorpus, removeWords, stopwords('english'))
@@ -106,7 +99,7 @@ PlotWordCloud <- function(searchTerm, maxTweets)
   tweetCorpus <- tm_map(tweetCorpus, removeWords, stopwords('french'))  
   
   
-  #-----Statistical analysis------
+  #-----Calculate frequencies------
   
   # Make a matrix whose entry (i,j) indicates the number of times 
   # that word i appears in tweet j.  
@@ -116,7 +109,7 @@ PlotWordCloud <- function(searchTerm, maxTweets)
   # Compute words' frequency by adding entries in each row,
   # and order words from high freq to low freq.
   sortedMatrix <- sort(rowSums(tdMatrix), decreasing=TRUE)      
-  # Save words and computed frequencies in a data frame.
+  # Save words and computed frequencies in a data frame
   cloudFrame <- data.frame(word = names(sortedMatrix), freq = sortedMatrix)   
   
   
@@ -125,7 +118,7 @@ PlotWordCloud <- function(searchTerm, maxTweets)
   # Set up text color (Red-Purple).
   palette <- brewer.pal(9,"RdPu")
   palette <- palette[-(1:4)] 
-  # Print wordcloud.
+  # Plot word cloud.
   wordcloud(cloudFrame$word, cloudFrame$freq, scale=c(4,.3),,max.words=25,random.order=FALSE,,rot.per=.25,colors=palette)
 }
 
